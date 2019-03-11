135ae7e4f71bb2e50ecdf997cb997f9e214f9e47
AroPho <aaronnguyen2009@yahoo.com>
2019-03-11 11:37:57 -0700

    Started pa5

---
+++ pa5/cmake-build-debug/CMakeFiles/pa5.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/aaron/Desktop/School/CS12B/src/pa5/Dictionary.c" "C:/Users/aaron/Desktop/School/CS12B/src/pa5/cmake-build-debug/CMakeFiles/pa5.dir/Dictionary.c.obj"
+  "C:/Users/aaron/Desktop/School/CS12B/src/pa5/DictionaryClient.c" "C:/Users/aaron/Desktop/School/CS12B/src/pa5/cmake-build-debug/CMakeFiles/pa5.dir/DictionaryClient.c.obj"
+  "C:/Users/aaron/Desktop/School/CS12B/src/pa5/main.c" "C:/Users/aaron/Desktop/School/CS12B/src/pa5/cmake-build-debug/CMakeFiles/pa5.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pa5/cmake-build-debug/CMakeFiles/pa5.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pa5.dir/main.c.obj"
+  "CMakeFiles/pa5.dir/Dictionary.c.obj"
+  "CMakeFiles/pa5.dir/DictionaryClient.c.obj"
+  "pa5.pdb"
+  "pa5.exe"
+  "pa5.exe.manifest"
+  "libpa5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pa5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
